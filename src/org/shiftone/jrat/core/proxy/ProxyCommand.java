bace5f395136650431fc351f9e898c0ff61e3122
jeffdrost <jeffdrost@51b2fdc4-721d-0410-97db-d1d35c42e7b1>
2007-05-29 13:59:21 +0000

    git-svn-id: http://svn.code.sf.net/p/jrat/code/trunk@29 51b2fdc4-721d-0410-97db-d1d35c42e7b1

---
+++ src/org/shiftone/jrat/core/proxy/ProxyCommand.java
@@ -0,0 +1,31 @@
+package org.shiftone.jrat.core.proxy;
+
+
+
+import org.shiftone.jrat.api.Command;
+
+import java.lang.reflect.Method;
+
+
+/**
+ * @author $Author: jeffdrost $
+ * @version $Revision: 1.1 $
+ */
+public class ProxyCommand implements Command {
+
+    private final Object   target;
+    private final Method   method;
+    private final Object[] args;
+
+    public ProxyCommand(Object target, Method method, Object[] args) {
+
+        this.target = target;
+        this.method = method;
+        this.args   = args;
+    }
+
+
+    public Object execute() throws Throwable {
+        return method.invoke(target, args);
+    }
+}
