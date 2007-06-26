968eadcbbfed266634b3722302300e97b979ab5e
jeffdrost <jeffdrost@51b2fdc4-721d-0410-97db-d1d35c42e7b1>
2007-06-26 11:44:50 +0000

    moving to java serialization
    git-svn-id: http://svn.code.sf.net/p/jrat/code/trunk@79 51b2fdc4-721d-0410-97db-d1d35c42e7b1

--- src/org/shiftone/jrat/provider/tree/ui/MainViewPanel.java
+++ src/org/shiftone/jrat/provider/tree/ui/MainViewPanel.java
@@ -2,7 +2,8 @@ package org.shiftone.jrat.provider.tree.ui;
 
 import org.shiftone.jrat.provider.tree.StackNode;
 import org.shiftone.jrat.provider.tree.ui.trace.TreeViewerPanel;
-import org.shiftone.jrat.provider.tree.ui.hierarchy.GraphViewPanel;
+import org.shiftone.jrat.provider.tree.ui.graph.GraphViewPanel;
+import org.shiftone.jrat.util.log.Logger;
 
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
@@ -12,7 +13,7 @@ import java.awt.BorderLayout;
  * @Author Jeff Drost
  */
 public class MainViewPanel extends JPanel {
-
+    private static final Logger LOG = Logger.getLogger(MainViewPanel.class);
     private final JTabbedPane tabbedPane = new JTabbedPane();
     private final StackNode rootNode;
 
@@ -21,7 +22,9 @@ public class MainViewPanel extends JPanel {
         setLayout(new BorderLayout());
         add(tabbedPane, BorderLayout.CENTER);
 
+        LOG.info("creating Trace");
         tabbedPane.addTab("Trace", new TreeViewerPanel(rootNode));
+        LOG.info("creating Hierarchy");
         tabbedPane.addTab("Hierarchy", new GraphViewPanel(rootNode));
     }
 
