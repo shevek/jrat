2d3a568e6c19ee25e54e29d22f412807a36a682d
jeffdrost <jeffdrost@51b2fdc4-721d-0410-97db-d1d35c42e7b1>
2007-08-29 14:49:06 +0000

    git-svn-id: http://svn.code.sf.net/p/jrat/code/trunk@125 51b2fdc4-721d-0410-97db-d1d35c42e7b1

---
+++ src/org/shiftone/jrat/provider/tree/ui/hierarchy/nodes/MethodHierarchyNode.java
@@ -0,0 +1,44 @@
+package org.shiftone.jrat.provider.tree.ui.hierarchy.nodes;
+
+import org.shiftone.jrat.core.MethodKey;
+
+import java.util.List;
+import java.util.Collections;
+
+/**
+ * @author (jeff@shiftone.org) Jeff Drost
+ */
+public class MethodHierarchyNode extends HierarchyNode {
+
+    public MethodHierarchyNode(MethodKey methodKey) {
+
+        super(methodKey.getMethodName()
+                + "("
+                + methodKey.getSig().getShortText()
+                + ")");
+    }
+
+    public void finalizeStatistics() {
+        // do nothing 
+    }
+
+    public boolean isExecuted() {
+        return false;
+    }
+
+    public long getTotalDurationMs() {
+        return 0;
+    }
+
+    public int getTotalMethods() {
+        return 1;
+    }
+
+    public int getExecutedMethods() {
+        return 0;
+    }
+
+    public List getChildren() {
+        return Collections.emptyList();
+    }
+}
