77a860ca9f1bc7a9d410f29fb7692e0e9feb59c1
Shevek <shevek@anarres.org>
2014-05-10 16:20:32 -0700

    Initial gradle port.

---
+++ jrat-agent/src/main/java/org/shiftone/jrat/provider/tree/TreeMethodHandlerFactory.java
@@ -0,0 +1,123 @@
+package org.shiftone.jrat.provider.tree;
+
+
+import org.shiftone.jrat.core.MethodKey;
+import org.shiftone.jrat.core.spi.AbstractMethodHandlerFactory;
+import org.shiftone.jrat.core.spi.MethodHandler;
+import org.shiftone.jrat.core.spi.RuntimeContext;
+import org.shiftone.jrat.provider.tree.command.DumpOutputCommandlet;
+import org.shiftone.jrat.provider.tree.command.ResetCommandlet;
+import org.shiftone.jrat.provider.tree.command.WriteOutputCommandlet;
+// import org.shiftone.jrat.provider.tree.ui.TraceViewBuilder;
+import org.shiftone.jrat.provider.tree.TreeWebActionFactory;
+import org.shiftone.jrat.util.AtomicLong;
+import org.shiftone.jrat.util.log.Logger;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+
+
+/**
+ * Class TreeMethodHandlerFactory
+ *
+ * @author jeff@shiftone.org (Jeff Drost)
+ */
+public class TreeMethodHandlerFactory extends AbstractMethodHandlerFactory implements TreeMethodHandlerFactoryMBean {
+
+    private static final Logger LOG = Logger.getLogger(TreeMethodHandlerFactory.class);
+    private final TreeNode rootNode = new TreeNode();
+    private final Set allMethodKeys = new HashSet();
+    private final DelegateThreadLocal delegateThreadLocal = new DelegateThreadLocal(this);
+    private final AtomicLong methodHandlerCount = new AtomicLong();
+    private final List treeNodes = new ArrayList(); /* <TreeNode> */
+    private final TreeWebActionFactory webActionFactory = new TreeWebActionFactory(treeNodes);
+
+    public void startup(RuntimeContext context) throws Exception {
+        super.startup(context);
+        context.registerMBean(this);
+        context.register(new ResetCommandlet(this));
+        context.register(new WriteOutputCommandlet(this));
+        context.register(new DumpOutputCommandlet(this));
+
+        context.registerWebActionFactory(webActionFactory);
+    }
+
+
+    public synchronized final MethodHandler createMethodHandler(MethodKey methodKey) {
+
+        methodHandlerCount.incrementAndGet();
+        allMethodKeys.add(methodKey);
+
+        return new TreeMethodHandler(this, methodKey);
+    }
+
+
+    public long getMethodHandlerCount() {
+        return methodHandlerCount.get();
+    }
+
+    public TreeNode createTreeNode(MethodKey methodKey, TreeNode treeNode) {
+        TreeNode newNode = new TreeNode(methodKey, treeNode);
+        // this add makes the web factory able to look up a node by id.
+        treeNodes.add(treeNode);
+        return newNode;
+    }
+
+    /**
+     * Returns the current thread's delegate instance. This delegate will
+     * operate on this factory's call tree data structure when events are
+     * processed.
+     */
+    public final Delegate getDelegate() {
+        return (Delegate) delegateThreadLocal.get();
+    }
+
+
+    public final TreeNode getRootNode() {
+        return rootNode;
+    }
+
+
+    public synchronized void writeOutputFile() {
+        writeOutputFile(getOutputFile());
+    }
+
+    public synchronized void reset() {
+        rootNode.reset();
+    }
+
+    public void writeOutputFile(String fileName) {
+
+        LOG.info("writeOutputFile...");
+
+		/* TODO
+        getContext().writeSerializable(fileName,
+                new TraceViewBuilder(
+                        rootNode,
+                        new HashSet(allMethodKeys), // copy to avoid sync issues
+                        getContext().getStartTimeMs(),
+                        System.currentTimeMillis(),
+                        getContext().getSystemPropertiesAtStartup(),
+                        getContext().getHostName(),
+                        getContext().getHostAddress()
+                )
+        );
+*/
+
+    }
+
+
+    public void shutdown() {
+
+        LOG.info("shutdown...");
+        writeOutputFile();
+        LOG.info("shutdown complete");
+    }
+
+
+    public String toString() {
+        return "Tree Handler Factory";
+    }
+}
